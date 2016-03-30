b399faced92a6b83a2625cb3abfa0667adc59d75
Control Zeta <francisco.arroyo@controlzeta.com.mx>
2016-03-29 18:30:32 -0600

    Inicio de proyecto de implementación
    Con el objetivo de estandarizar la estructura de los contenidos en las
    dependencias, se ha realizado el siguiente documento, que funciona como
    una guía compuesta por un conjunto de elementos específicos que se deben
    de cumplir al momento de realizar la maquetación de los trámites en
    línea.

---
+++ GobMxDummy/GobMxDummy/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GobMxDummy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Bienvenido a nuestra aplicación Dummy con Gob Mx.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Bienvenido a nuestra aplicación Dummy con Gob Mx.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Bienvenido a nuestra aplicación Dummy con Gob Mx.";
+
+            return View();
+        }
+    }
+}
